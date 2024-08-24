899930fd32187c617c6b70fb81517dc3dfde9a2f
Mauricio Aguirre <mauricioaguirrert@yahoo.com>
2024-08-24 13:48:35 -0300

    tio lico 1

---
+++ formulario-whatsapp-master (1)/formulario-whatsapp-master/assets/js/whatsapp.js
@@ -0,0 +1,36 @@
+/* 
+* Author: Gabriel Chávez
+* Website: https://gabrielchavez.me
+*/
+
+const $form = document.querySelector('#form');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const phone = '5493868461430';
+
+
+$form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    buttonSubmit.disabled = true
+
+    setTimeout(() => {
+        let name = document.querySelector('#name').value
+        let direccion = document.querySelector('#direccion').value
+        let pedido = document.querySelector('#pedido').value
+        let message = 'send?phone=' + phone + '&text=*_Pedido para EL TIO RICO_*%0A*Datos del cliente*%0A*¿Cual es tu nombre?*%0A' + name + '%0A*¿Cuál es tu dirección?*%0A' + direccion + '%0A*¿Cuál es tu pedido?*%0A' + pedido + ''
+
+
+        if (isMobile()) {
+            window.open(urlMobile + message, '_blank')
+        } else {
+            window.open(urlDesktop + message, '_blank')
+        }
+
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+
+    }, 4000);
+
+});
